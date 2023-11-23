d5a5e2950787bb2d6835d3093dc0c15f978d094a
Omar <omarta@code.edu.az>
2023-11-23 12:42:08 +0400

    Added Models

---
+++ Final_Project_User_Login/Domain/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Student : BaseEntity
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+    }
+}
