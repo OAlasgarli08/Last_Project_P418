21f5a32f9d00a491b864b6cae6f6c81782c04a42
Omar <omarta@code.edu.az>
2023-11-24 21:47:41 +0400

    Added Controller files

---
+++ Final_Project_User_Login/Final_Project_User_Login/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project_User_Login.Controllers
+{
+    internal class UserController
+    {
+    }
+}
